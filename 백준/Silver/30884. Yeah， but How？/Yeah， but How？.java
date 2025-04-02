27010693d814076a50efc8e8e0e70ddca9995925
KANG SE PIL <122997638+ksp0814@users.noreply.github.com>
2025-04-02 16:37:17 +0900

    [Silver III] Title: Yeah, but How?, Time: 244 ms, Memory: 20084 KB -BaekjoonHub

--- 백준/Silver/30884. Yeah， but How？/Yeah， but How？.java
+++ 백준/Silver/30884. Yeah， but How？/Yeah， but How？.java
@@ -1,58 +1,27 @@
-
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
         StringBuilder sb = new StringBuilder();
 
-        String input = br.readLine();
+        for(int i = 0; i < s.length() - 1; i++) {
+            sb.append(s.charAt(i));
 
-        System.out.println(Solution(input));
-    }
-
-    static String Solution(String input) {
-        Deque<Character> stack = new ArrayDeque<>();
-        StringBuilder sb = new StringBuilder();
-
-        if (input.length() == 2) {
-            return "(1+1)";
-        }
-
-        for (int i = 0; i < input.length(); i++) {
-            stack.offerLast(input.charAt(i));
-        }
-
-        boolean check = true;
-
-        while (check) {
-            if (stack.size()==2) {
-                check = false;
-            }
-
-            char L = stack.pollFirst();
-            char R = stack.pollFirst();
-
-            if (L == ')' && R == '(') {
-                sb.append(L);
+            if( s.charAt(i) == '(' && s.charAt(i+1) == ')'){
+                sb.append('1');
+            } else if (s.charAt(i) ==')' && s.charAt(i+1) == '('){
                 sb.append('+');
             }
-
-            else if (L == '(' && R == ')') {
-                sb.append(L);
-                sb.append('1');
-            } else {
-                sb.append(L);
-            }
-            stack.offerFirst(R);
-            if (check == false) sb.append(R);
         }
-        String result = String.valueOf(sb);
-        return result;
+        sb.append(s.charAt(s.length()-1));
+
+        System.out.println(sb.toString());
+        
     }
 }
