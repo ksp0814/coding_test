51c38b6b3a096807ce71ed94cbb9a4638def111e
KANG SE PIL <122997638+ksp0814@users.noreply.github.com>
2025-05-16 23:25:50 +0900

    [D3] Title: 수의 새로운 연산, Time: 203 ms, Memory: 40,652 KB -BaekjoonHub

--- SWEA/D3/1493. 수의 새로운 연산/수의 새로운 연산.java
+++ SWEA/D3/1493. 수의 새로운 연산/수의 새로운 연산.java
@@ -1,59 +1,45 @@
 import java.util.Scanner;
 
 class Solution {
-    static class Coords {
-        int x, y;
-        public Coords(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    // 1-기반 좌표 (x,y)로부터 숫자 N 찾기
-    public static int getValue(int x, int y) {
-        int diag = x + y - 1; // (x+y-2)*(x+y-1)/2 + x 공식 사용 시
-        int prevCount = (diag - 1) * diag / 2;
-        return prevCount + x;
-    }
-
-    // 숫자 N으로부터 1-기반 좌표 (x,y) 찾기
-    public static Coords getCoords(int val) {
-        int diagBase = 0; 
-        // val이 (diagBase)번째 대각선 그룹에 속하도록 diagBase 찾기
-        // (diagBase-1)-번째 대각선 그룹까지의 숫자 개수: (diagBase-1)*diagBase/2
-        // diagBase-번째 대각선 그룹까지의 숫자 개수: diagBase*(diagBase+1)/2
-        while (true) {
-            if (diagBase * (diagBase + 1) / 2 >= val) {
-                break;
-            }
-            diagBase++;
-        }
-        
-        int prevCount = (diagBase - 1) * diagBase / 2;
-        int x = val - prevCount;
-        int y = diagBase - x + 1; // x + y -1 = diagBase  => y = diagBase - x + 1
-        return new Coords(x, y);
-    }
-
     public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
-        int T;
-        T = sc.nextInt();
+        int T = sc.nextInt();
 
         for (int test_case = 1; test_case <= T; test_case++) {
-            int p_val = sc.nextInt();
-            int q_val = sc.nextInt();
+            int p = sc.nextInt();
+            int q = sc.nextInt();
 
-            Coords coords_p = getCoords(p_val);
-            Coords coords_q = getCoords(q_val);
+            int[] posP = getCoordinate(p);
+            int[] posQ = getCoordinate(q);
 
-            int new_x = coords_p.x + coords_q.x;
-            int new_y = coords_p.y + coords_q.y;
+            int x = posP[0] + posQ[0];
+            int y = posP[1] + posQ[1];
 
-            int result = getValue(new_x, new_y);
-
-            System.out.println("#" + test_case + " " + result);
+            int result = getValue(x, y);
+            System.out.printf("#%d %d\n", test_case, result);
         }
-        sc.close();
+    }
+
+    // 좌표에서 숫자로
+    static int getValue(int x, int y) {
+        int sum = x + y - 1;
+        return (sum * (sum - 1)) / 2 + x;
+    }
+
+    // 숫자에서 좌표로
+    static int[] getCoordinate(int value) {
+        int sum = 1;
+        int line = 1;
+
+        while (sum < value) {
+            line++;
+            sum += line;
+        }
+
+        int diff = sum - value;
+        int x = line - diff;
+        int y = 1 + diff;
+
+        return new int[]{x, y};
     }
 }
