d574c19bf109298789cfe340bf6671c4f07c04e8
KANG SE PIL <122997638+ksp0814@users.noreply.github.com>
2025-05-19 19:00:36 +0900

    [D3] Title: 진기의 최고급 붕어빵, Time: 348 ms, Memory: 101,336 KB -BaekjoonHub

--- SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.java
+++ SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.java
@@ -5,45 +5,49 @@ class Solution
 {
 	public static void main(String args[]) throws Exception
 	{
-		
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
+		        Scanner sc = new Scanner(System.in);
 
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			int n = sc.nextInt();
+        int T  = sc.nextInt();
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+
+            int n = sc.nextInt();
             int m = sc.nextInt();
             int k = sc.nextInt();
-            
-            int[] list = new int[n];
-            int[] dp = new int[11111];
-            
-            for (int i =0; i < n; i++) {
-            	list[i] = sc.nextInt();
+
+            int[] list = new int[11111];
+
+            int[] person = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                person[i] = sc.nextInt();
             }
-            Arrays.sort(list);
-            
-            for(int i =m; i < 11111; i += m ){
-            	dp[i] = k;
+            Arrays.sort(person);
+
+            int x = 0;
+
+            for (int i = 1; i < 11111; i++) {
+                if(i % m == 0) {
+                    x += k;
+                    list[i] = x;
+                } else list[i] = x;
             }
-            
+
             boolean check = true;
-            
-            for(int i =0; i < n; i++) {
-             	int time = list[i];
-                int bread = (time / m) * k;
-                
-                if( bread < i+1) {
-                	check = false;
-                    break;
-                }
-             
+
+            for (int i = 0; i < n; i++) {
+                if (list[person[i]] >= i +1) {
+                    check = true;
+                } else {check = false;
+                break;}
             }
-            
+
             if(check) {
-            System.out.println("#" +test_case + " Possible");
-            } else System.out.println("#" + test_case + " Impossible" );
-	}
+            System.out.println("#" + test_case + " Possible");
+            } else {
+                System.out.println("#" + test_case + " Impossible");
+            }
+
+}
 }
 }
