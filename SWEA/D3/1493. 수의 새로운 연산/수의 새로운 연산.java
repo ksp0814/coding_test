91ccda290b2d4c37dec4105c76c93422c1e3edd2
KANG SE PIL <122997638+ksp0814@users.noreply.github.com>
2025-05-19 18:03:21 +0900

    [D3] Title: 수의 새로운 연산, Time: 448 ms, Memory: 32,224 KB -BaekjoonHub

--- SWEA/D3/1493. 수의 새로운 연산/수의 새로운 연산.java
+++ SWEA/D3/1493. 수의 새로운 연산/수의 새로운 연산.java
@@ -9,49 +9,49 @@ class Solution
 	{
 
 		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-        
-                int[][] list = new int[300][300];
-        list[0][0] = 1;
 
-        for(int i = 1; i < 300; i++){
-            list[i][0] = list[i-1][0]+i;
+        int T = sc.nextInt();
+
+        int[][] list = new int[300][300];
+
+        list[0][0] = 1;
+        for (int i =1; i < 300; i++) {
+            list[i][0] = list[i-1][0] + i;
         }
 
-        for(int r= 0; r < 300; r++){
-            for(int c = 1; c < 300; c++){
-                list[r][c] = list[r][c-1] + (r+c+1);
+        for (int i = 0; i <300; i++) {
+            for (int j = 1; j <300; j++) {
+                list[i][j] = list[i][j-1] + (i+j+1);
             }
         }
 
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-             int p = sc.nextInt();
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+
+            int p = sc.nextInt();
             int q = sc.nextInt();
+            int x=0;
+            int y=0;
+            int z=0;
+            int w=0;
 
-            int[] p_idx = new int[2];
-            int[] q_idx = new int[2];
-
-            for(int r = 0; r < 300; r++){
-                for(int c = 0; c < 300; c++){
-                    if(list[r][c] == p){
-                        p_idx[0] = r+1;
-                        p_idx[1] = c+1;
+            for (int i = 0; i < 300; i++) {
+                for (int j = 0; j < 300; j++) {
+                    if(list[i][j] == p) {
+                        x = i;
+                        y = j;
                     }
-                    if(list[r][c] == q){
-                        q_idx[0] = r+1;
-                        q_idx[1] = c+1;
+                    if (list[i][j] == q) {
+                        z = i;
+                        w = j;
                     }
                 }
             }
 
-            int target_x = p_idx[0]+q_idx[0];
-            int target_y = p_idx[1]+q_idx[1];
+            int a = x+z +1;
+            int b = y+w +1;
 
-            int result = list[target_x-1][target_y-1];
-
-            System.out.println("#" + test_case + " " + result);
+            System.out.println("#" + test_case +" " + list[a][b]);
 		
 
 		}
