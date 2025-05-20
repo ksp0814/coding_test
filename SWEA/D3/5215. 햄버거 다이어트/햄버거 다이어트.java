dac64a248b9e909b6399141460e96d0ba2e1000c
KANG SE PIL <122997638+ksp0814@users.noreply.github.com>
2025-05-21 00:53:15 +0900

    [D3] Title: 햄버거 다이어트, Time: 139 ms, Memory: 40,284 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -11,35 +11,36 @@ class Solution
 		int T;
 		T=sc.nextInt();
 
-
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
 			int N = sc.nextInt();
             int L = sc.nextInt();
             
-            int[][] list = new int[N][2];
+            int[][] list  = new int[N][2];
             
-            for(int i =0; i<N; i++) {
+            for (int i =0; i< N; i++) {
             	list[i][0] = sc.nextInt();
                 list[i][1] = sc.nextInt();
             }
             
             int[][] dp = new int[N+1][L+1];
             
-            for(int i =1; i <= N; i++) {
-            	int score = list[i-1][0];
-                int cal = list[i-1][1];
-                
-                for ( int k = 0; k <= L; k++) {
-                	if(k >= cal) {
-                    	dp[i][k] = Math.max(dp[i-1][k],dp[i-1][k-cal] + score);
+            for(int i =1; i<=N; i++) {
+                int score = list[i-1][0];
+                int calorie = list[i-1][1]; 
+            	for(int j =0; j <=L; j++) {
+                	if( j >= calorie) {
+                    	dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-calorie] + score);
                     } else {
-                    	dp[i][k] = dp[i-1][k];
+                    	dp[i][j] = dp[i-1][j];
                     }
                 }
             }
             
-            System.out.println("#" +test_case + " " +dp[N][L]);
+            System.out.println("#" + test_case + " " + dp[N][L]);
+            
+            
+			
 		}
 	}
 }
