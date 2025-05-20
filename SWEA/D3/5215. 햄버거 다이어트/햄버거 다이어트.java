e71b38c377c59de93e70d010f7a50163ee936b81
KANG SE PIL <122997638+ksp0814@users.noreply.github.com>
2025-05-20 18:47:59 +0900

    [D3] Title: 햄버거 다이어트, Time: 151 ms, Memory: 40,416 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -4,46 +4,42 @@ import java.io.FileInputStream;
 
 class Solution
 {
-    static int[][] list;
-    static int max;
-    static int N;
-    static int L;
 	public static void main(String args[]) throws Exception
 	{
+
 		Scanner sc = new Scanner(System.in);
 		int T;
 		T=sc.nextInt();
 
+
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
-
-            N = sc.nextInt();
-            L = sc.nextInt();
+			int N = sc.nextInt();
+            int L = sc.nextInt();
             
-            list = new int[N][2];
+            int[][] list = new int[N][2];
             
-            for(int i =0; i < N; i++) {
+            for(int i =0; i<N; i++) {
             	list[i][0] = sc.nextInt();
                 list[i][1] = sc.nextInt();
             }
-            max = 0;
-            dfs(0,0,0);
             
-            System.out.println("#" + test_case + " " + max);
+            int[][] dp = new int[N+1][L+1];
+            
+            for(int i =1; i <= N; i++) {
+            	int score = list[i-1][0];
+                int cal = list[i-1][1];
+                
+                for ( int k = 0; k <= L; k++) {
+                	if(k >= cal) {
+                    	dp[i][k] = Math.max(dp[i-1][k],dp[i-1][k-cal] + score);
+                    } else {
+                    	dp[i][k] = dp[i-1][k];
+                    }
+                }
+            }
+            
+            System.out.println("#" +test_case + " " +dp[N][L]);
 		}
 	}
-    
-    static void dfs(int idx, int tasteSum, int calSum) {
-		if( calSum > L) return;        
-        if(idx == N) {
-        	max = Math.max(max, tasteSum);
-            return;
-        }
-        
-         // 현재 재료 선택
-        dfs(idx + 1, tasteSum + list[idx][0], calSum + list[idx][1]);
-
-        // 현재 재료 선택하지 않음
-        dfs(idx + 1, tasteSum, calSum);
-    }
 }
