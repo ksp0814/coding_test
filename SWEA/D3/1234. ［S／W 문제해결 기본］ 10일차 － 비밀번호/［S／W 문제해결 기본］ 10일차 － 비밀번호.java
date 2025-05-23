618f711a06aa7a14b3e224e9088232fa169c81d9
KANG SE PIL <122997638+ksp0814@users.noreply.github.com>
2025-05-23 15:24:07 +0900

    [D3] Title: [S/W 문제해결 기본] 10일차 - 비밀번호, Time: 94 ms, Memory: 26,624 KB -BaekjoonHub

--- SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.java
+++ SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.java
@@ -12,25 +12,35 @@ class Solution
 
 		for(int test_case = 1; test_case <= 10; test_case++)
 		{
-			int N =sc.nextInt();
+		
+            int T = sc.nextInt();
             
-            Stack<Character> list = new Stack<>();
+            List<Integer> list = new ArrayList<>();
             
             String s = sc.next();
+            
+            list.add(s.charAt(0) - '0');
            
-            
-            for(int i =0; i < N; i++) {
-               char c = s.charAt(i);
-               if( !list.empty() && list.peek() == c) { // list가 비어잇지 않고, c가 list에 마지막 값과 같다면 리스트에 마지막 값을 지운다.
-                   list.pop();
-               } else list.push(c); // 아니라면 리스트에 c를 추가한다.
+            int x = 0;
+            for(int i =1; i < T; i++) {
+            	if( x >= 0 && list.get(x) == s.charAt(i) -'0') {
+                	list.remove(x);
+                    x--;
+                } else {
+                	list.add(s.charAt(i) -'0');
+                    x++;
+                }
             }
-            System.out.print("#" + test_case + " ");
             
-            for(int i =0; i < list.size(); i++){
+            System.out.print("#" + test_case +" ");
+            
+            for(int i =0; i < list.size(); i++) {
             	System.out.print(list.get(i));
             }
+            
             System.out.println();
+            
+
 		}
 	}
 }
