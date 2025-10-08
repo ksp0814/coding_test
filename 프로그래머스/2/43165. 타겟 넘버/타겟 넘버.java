70a092ddf5c13743f7ee222b87d21a20366a35e1
KANG SE PIL <122997638+ksp0814@users.noreply.github.com>
2025-10-09 00:54:14 +0900

    [level 2] Title: 타겟 넘버, Time: 8.31 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,26 +1,23 @@
-public class Solution {
-    int[] numbers;
-    int ans;
-    int target;
-
-
-    void dfs(int index, int sum) {
-
+class Solution {
+    int answer;
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+        
+        dfs(numbers,0,0,target);
+        
+        
+        return answer;
+    }
+    
+    void dfs(int[] numbers,int index, int sum, int res) {
         if(index == numbers.length) {
-            if(sum == target) ans++;
+            if(sum == res) {
+                answer++;
+            }
             return;
         }
-
-        dfs(index+1, sum + numbers[index]);
-        dfs(index+1, sum - numbers[index]);
-    }
-    public int solution(int[] numbers, int target){
-        ans = 0;
-        this.numbers = numbers;
-        this.target = target;
-
-        dfs(0,0);
-
-        return ans;
+        
+        dfs(numbers,index+1, sum + numbers[index],res);
+        dfs(numbers,index+1, sum - numbers[index],res);
     }
 }
