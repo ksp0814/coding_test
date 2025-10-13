a1d1f1a61e95de5288646ef0571c1d9ac439199f
KANG SE PIL <122997638+ksp0814@users.noreply.github.com>
2025-10-13 14:33:10 +0900

    [Silver I] Title: 오르막 수, Time: 180 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int mod = 10007;
+
+        int[][] dp = new int[n+1][10];
+
+        for(int i = 0; i < 10; i++) {
+            dp[1][i]=1;
+        }
+
+        for(int i = 2; i <= n; i++) {
+            for(int j = 0; j < 10; j++) {
+                for(int k = 0; k <= j; k++) {
+                    dp[i][j] += dp[i-1][k];
+                    dp[i][j] %= mod;
+                }
+            }
+        }
+
+        int ans = 0;
+        for(int i = 0; i<10; i++) {
+            ans += dp[n][i];
+            ans %= mod;
+        }
+
+        System.out.println(ans);
+    }
+
+}
