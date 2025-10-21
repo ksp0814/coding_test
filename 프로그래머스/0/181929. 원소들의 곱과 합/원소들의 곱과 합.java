8fbb32171e8f3085357f4326e497f4c5ca4913a9
KANG SE PIL <122997638+ksp0814@users.noreply.github.com>
2025-10-21 18:44:09 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        int sum = 0;
+        int num = 1;
+        
+        for(int i = 0; i < num_list.length; i++) {
+            sum += num_list[i];
+            num *= num_list[i];
+        }
+        
+        sum = sum * sum;
+        
+        if( num < sum) {
+            answer = 1;
+        } else answer = 0;
+        return answer;
+    }
+}
