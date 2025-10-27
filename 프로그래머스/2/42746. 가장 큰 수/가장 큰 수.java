57898d20036944534fad3a31801bb4d4d2079818
KANG SE PIL <122997638+ksp0814@users.noreply.github.com>
2025-10-27 14:35:30 +0900

    [level 2] Title: 가장 큰 수, Time: 3216.36 ms, Memory: 409 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,25 +1,18 @@
 import java.util.*;
-
 class Solution {
     public String solution(int[] numbers) {
         String answer = "";
-        
         String[] str = new String[numbers.length];
         
-        for (int i =0; i < numbers.length; i++) {
+        for(int i = 0; i < numbers.length; i++) {
             str[i] = String.valueOf(numbers[i]);
         }
         
-        Arrays.sort(str, (a,b) -> (b+a).compareTo(a+b)); // 내림차순
+        Arrays.sort(str,(a,b) -> (b+a).compareTo(a+b));
         
-        if(str[0].equals("0")) {
-            return "0";
-        }
+        if(str[0].equals("0")) return "0";
         
-        for(String s : str) {
-            answer += s;
-        }
-        
+        for(String s : str) answer+=s;
         return answer;
     }
 }
